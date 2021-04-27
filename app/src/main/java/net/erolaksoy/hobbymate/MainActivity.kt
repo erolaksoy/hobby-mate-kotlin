685fe0185f68f0272da67b19cb37faf5db894305
aksoyerol <erolaksoy98@gmail.com>
2021-04-27 18:50:23 +0300

    Edit DrawerLayout, ActivityMain

--- app/src/main/java/net/erolaksoy/hobbymate/MainActivity.kt
+++ app/src/main/java/net/erolaksoy/hobbymate/MainActivity.kt
@@ -1,25 +1,26 @@
 package net.erolaksoy.hobbymate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import net.erolaksoy.hobbymate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(this.layoutInflater) }
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
         toggle = ActionBarDrawerToggle(
             this,
@@ -27,8 +28,17 @@ class MainActivity : AppCompatActivity() {
             R.string.bottom_sheet_behavior,
             R.string.app_name
         )
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
         binding.toolbar.menuIcon.setOnClickListener { binding.drawerLayout.open() }
     }
+
 }
--- app/src/main/java/net/erolaksoy/hobbymate/ui/home/HomeFragment.kt
+++ app/src/main/java/net/erolaksoy/hobbymate/ui/home/HomeFragment.kt
@@ -1,19 +1,13 @@
 package net.erolaksoy.hobbymate.ui.home
 
 import android.os.Bundle
-import android.view.*
-import android.widget.ImageView
-import android.widget.Toast
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import net.erolaksoy.hobbymate.R
 import net.erolaksoy.hobbymate.databinding.FragmentHomeBinding
-
 import java.util.*
 
 class HomeFragment : Fragment() {
@@ -22,17 +16,9 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        setHasOptionsMenu(true)
+
         val binding: FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
         LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        val act = activity as AppCompatActivity
-
-        val toolbar = view?.findViewById<Toolbar>(R.id.toolbar)
-        toolbar?.setNavigationIcon(R.drawable.menu_ico)
-        toolbar?.inflateMenu(R.menu.bottom_menu)
-        act.setSupportActionBar(toolbar)
-
-
         val list: ArrayList<String> = arrayListOf(
             "Lorem Ipsum",
             "has been",
@@ -43,29 +29,13 @@ class HomeFragment : Fragment() {
             "took a galley",
             "of type and scrambled"
         )
-
         binding.popularRecyclerView.adapter = HomePagePopularAdapter(list)
-
-//        binding.apply {
-//            toolbar.menuIcon.setOnClickListener {
-//                Toast.makeText(requireContext(), "This is a menu icon", Toast.LENGTH_SHORT).show()
-//            }
-//            toolbar.userIcon.setOnClickListener {
-//                Toast.makeText(requireContext(), "This is a user icon", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-
         binding.recommendedRecyclerView.layoutManager =
             GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
         binding.recommendedRecyclerView.adapter = HomePageRecommendedAdapter(list)
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        activity?.menuInflater?.inflate(R.menu.bottom_menu,menu)
-
-    }
 
 }
 
